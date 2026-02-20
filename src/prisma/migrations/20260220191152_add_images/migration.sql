04f090d301037fea3c3071c4c1b04e1f7be659a9
Kartik Kumar <kartikprajapati5346@gmail.com>
2026-02-21 00:49:36 +0530

    feat: initialize SmartBus backend with Prisma schema and core models

---
+++ src/prisma/migrations/20260220191152_add_images/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "City" ADD COLUMN     "imageUrl" TEXT;
+
+-- AlterTable
+ALTER TABLE "Operator" ADD COLUMN     "logoUrl" TEXT;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "imageUrl" TEXT;
---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from "express"
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+const app = express();
+const port = process.env.PORT || 3000;
+
+dotenv.config();
+app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:5173", // your frontend
+    credentials: true
+}));
+app.use(helmet());
+
+app.get("/", (req, res) => {
+    res.send("Hello")
+})
+
+app.listen(port, () => {
+    console.log("Server started on port " + port)
+})
